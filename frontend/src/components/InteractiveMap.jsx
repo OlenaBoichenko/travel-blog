91bfe69f223aaf7597722146b3ff53b8ead61388
OlenaBoichenko <ol.boichenko.dev@gmail.com>
2024-12-23 18:41:38 -0600

    changed coordinates

--- frontend/src/components/InteractiveMap.jsx
+++ frontend/src/components/InteractiveMap.jsx
@@ -16,12 +16,12 @@ L.Icon.Default.mergeOptions({
 
 const locations = [
   { name: 'Ottawa, ON', coordinates: [45.4215, -75.6972], date: '2024-05-01' },
-  { name: 'North Bay, ON', coordinates: [46.3091, -79.4608], date: '2024-05-02' },
+  { name: 'Sudbury, ON', coordinates: [46.4917, -80.9930], date: '2024-05-02' },
   { name: 'Wawa, ON', coordinates: [47.9959, -84.7707], date: '2024-05-03' },
-  { name: 'Upsala, ON', coordinates: [49.0500, -90.4700], date: '2024-05-04' },
-  { name: 'Portage la Prairie, MB', coordinates: [49.9735, -98.2931], date: '2024-05-05' },
-  { name: 'Saskatoon, SK', coordinates: [52.1332, -106.6700], date: '2024-05-06' },
-  { name: 'Edmonton, AB', coordinates: [53.5461, -113.4938], date: '2024-05-07' },
+  { name: 'Dryden, ON', coordinates: [49.7801, -92.8370], date: '2024-05-04' },
+  { name: 'Minnedosa, MB', coordinates: [50.2471, -99.8386], date: '2024-05-05' },
+  { name: 'Langham, SK', coordinates: [52.3590, -106.9584], date: '2024-05-06' },
+  { name: 'Edson, AB', coordinates: [53.5854, -116.4337], date: '2024-05-07' },
   { name: 'Jasper, AB', coordinates: [52.8737, -118.0814], date: '2024-05-08' },
   { name: 'Kamloops, BC', coordinates: [50.6745, -120.3273], date: '2024-05-09' },
   { name: 'Vancouver, BC', coordinates: [49.2827, -123.1207], date: '2024-05-10' },
